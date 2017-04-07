c52bee1563ca8ad8514e7e54efea92877104ed47
gtxjinx <gtxmiracle@buaa.edu.cn>
2017-04-07 14:33:44 +0800

    完成回调接口

--- Gesture/app/src/main/java/com/example/administrator/gesture/WeChatView.java
+++ Gesture/app/src/main/java/com/example/administrator/gesture/WeChatView.java
@@ -6,6 +6,7 @@ import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.Toast;
 
 /**
  * Created by Jinx on 2017/4/6 11:07.
@@ -15,6 +16,7 @@ public class WeChatView extends ListView
 {
     private GestureDetector gestureDetector;
     private onFlingListener monFlingListener;
+    private Context context;
 
     class mGestureListener extends GestureDetector.SimpleOnGestureListener{
         @Override
@@ -22,11 +24,13 @@ public class WeChatView extends ListView
         {
             if(e1.getX()-e2.getX()>50)
             {
-                monFlingListener.onFlingLeft();
+                //monFlingListener.onFlingLeft();
+                Toast.makeText(context,"left",Toast.LENGTH_LONG).show();
             }
             else if (e2.getX()-e1.getX()>50)
             {
-                monFlingListener.onFlingRight();
+                //monFlingListener.onFlingRight();
+                Toast.makeText(context,"right",Toast.LENGTH_LONG).show();
             }
             return super.onFling(e1, e2, velocityX, velocityY);
         }
@@ -34,16 +38,26 @@ public class WeChatView extends ListView
     public WeChatView(Context context)
     {
         super(context);
+        this.context=context;
+        gestureDetector=new GestureDetector(context,new mGestureListener());
+        init();
+
     }
 
     public WeChatView(Context context, AttributeSet attrs)
     {
         super(context, attrs);
+        this.context=context;
+        gestureDetector=new GestureDetector(context,new mGestureListener());
+        init();
     }
 
     public WeChatView(Context context, AttributeSet attrs, int defStyleAttr)
     {
         super(context, attrs, defStyleAttr);
+        this.context=context;
+        gestureDetector=new GestureDetector(context,new mGestureListener());
+        init();
     }
 
     public void setonFlingListener(onFlingListener monFlingListener)
@@ -63,6 +77,7 @@ public class WeChatView extends ListView
             @Override
             public boolean onTouch(View v, MotionEvent event)
             {
+                if(event!=null)
                 gestureDetector.onTouchEvent(event);
                 return true;
             }
